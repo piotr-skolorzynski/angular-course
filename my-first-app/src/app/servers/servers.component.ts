5fbae1bb33f539394fc5ba45df683bbcce30034c
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-12-18 15:27:44 +0100

    passing and using data with event binding - one-way-binding

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -12,6 +12,7 @@ import { Component } from '@angular/core';
 export class ServersComponent {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
+  serverName = '';
 
   constructor() {
     setTimeout(() => (this.allowNewServer = true), 2000);
@@ -19,5 +20,9 @@ export class ServersComponent {
 
   onCreateServer() {
     this.serverCreationStatus = 'Server was created';
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }

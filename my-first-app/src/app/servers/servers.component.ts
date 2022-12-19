3e47eff8d82cf204dc040f288db027bcfa1f1bea
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-12-19 20:10:24 +0100

    modify servers component to show combination of using one-way two-way binfdings

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -19,7 +19,8 @@ export class ServersComponent {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created';
+    this.serverCreationStatus =
+      'Server was created! Name is ' + this.serverName;
   }
 
   onUpdateServerName(event: Event) {

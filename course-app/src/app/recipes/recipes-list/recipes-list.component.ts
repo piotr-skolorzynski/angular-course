6d2abe5cc63591ce43b52906b439da9aae226269
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-12-20 12:38:19 +0100

    modify recipes-list component create content for recipe item

--- course-app/src/app/recipes/recipes-list/recipes-list.component.ts
+++ course-app/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipes-list.component.css'],
 })
 export class RecipesListComponent {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'This is simply a test',
+      'https://www.istockphoto.com/photo/gingerbread-christmas-tree-on-dark-table-christmas-card-gm1355540092-430001551?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fchrismas-cake&utm_term=chrismas%20cake%3A%3A%3A'
+    ),
+  ];
 }

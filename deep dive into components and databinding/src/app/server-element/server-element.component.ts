6878d870b709af3e34800a4146d35039972c1d46
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-12-21 17:28:49 +0100

    add example how to use @ContentChild

--- deep dive into components and databinding/src/app/server-element/server-element.component.ts
+++ deep dive into components and databinding/src/app/server-element/server-element.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  ContentChild,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -8,6 +15,7 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class ServerElementComponent implements OnInit {
   @Input('srvElement') element: { type: string; name: string; content: string };
+  @ContentChild('contentParagraph', { static: true }) paragraph: ElementRef;
 
   constructor() {}
 

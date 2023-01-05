cb5f869ab67bed02da0f97e421068c40eaaecc98
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-01-05 10:00:35 +0100

    pass data and event binding - prop drilling

--- course-app/src/app/recipes/recipes-detail/recipes-detail.component.ts
+++ course-app/src/app/recipes/recipes-detail/recipes-detail.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-detail',
   templateUrl: './recipes-detail.component.html',
-  styleUrls: ['./recipes-detail.component.css']
+  styleUrls: ['./recipes-detail.component.css'],
 })
 export class RecipesDetailComponent {
-
+  @Input() recipe: Recipe;
 }
--- course-app/src/app/recipes/recipes-list/recipes-list.component.ts
+++ course-app/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,11 +7,21 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipes-list.component.css'],
 })
 export class RecipesListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
       'A test recipe',
       'This is simply a test',
       'https://food-images.files.bbci.co.uk/food/recipes/classic_christmas_cake_04076_16x9.jpg'
     ),
+    new Recipe(
+      'A test recipe',
+      'This is simply a test',
+      'https://food-images.files.bbci.co.uk/food/recipes/classic_christmas_cake_04076_16x9.jpg'
+    ),
   ];
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }

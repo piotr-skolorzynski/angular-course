29020b2c3fd122a0ab8bbe1f5cb111b2fa144714
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-01-09 12:48:46 +0100

    pass ingredients from resipes to shopping list via services

--- course-app/src/app/shared/dropdown.directive.ts
+++ course-app/src/app/shared/dropdown.directive.ts
@@ -11,16 +11,16 @@ import {
 export class DropdownDirective {
   @HostBinding('class.open') isOpen = false;
 
-  //   @HostListener('click') toggleOpen() {
-  //     this.isOpen = !this.isOpen;
-  //   }
+  @HostListener('click') toggleOpen() {
+    this.isOpen = !this.isOpen;
+  }
 
   //version with closing event outside the button click
-  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
-    this.isOpen = this.elRef.nativeElement.contains(event.target)
-      ? !this.isOpen
-      : false;
-  }
+  // @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+  //   this.isOpen = this.elRef.nativeElement.contains(event.target)
+  //     ? !this.isOpen
+  //     : false;
+  // }
 
   constructor(private elRef: ElementRef) {}
 }

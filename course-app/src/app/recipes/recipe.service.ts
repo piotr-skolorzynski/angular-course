3849258225dfca898722636506afc466c713bdbf
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-01-09 09:14:32 +0100

    use recipe service to cross-component comuniacation

--- course-app/src/app/recipes/recipe.service.ts
+++ course-app/src/app/recipes/recipe.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe(
       'A test recipe',

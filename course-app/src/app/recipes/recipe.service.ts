2c959fe71f45ec570ce7541e887156f25ddadb80
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-01-09 11:03:11 +0100

    add ingredients to recipe

--- course-app/src/app/recipes/recipe.service.ts
+++ course-app/src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
@@ -6,14 +7,16 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe(
-      'A test recipe',
-      'This is simply a test',
-      'https://food-images.files.bbci.co.uk/food/recipes/classic_christmas_cake_04076_16x9.jpg'
+      'A tasty Schnitzel',
+      'A super tasty Schnitzel - just awesome',
+      'https://food-images.files.bbci.co.uk/food/recipes/classic_christmas_cake_04076_16x9.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
-      'A test recipe 2',
-      'This is simply a test',
-      'https://food-images.files.bbci.co.uk/food/recipes/classic_christmas_cake_04076_16x9.jpg'
+      'Big Fat Burger',
+      'What else you need to say?',
+      'https://food-images.files.bbci.co.uk/food/recipes/classic_christmas_cake_04076_16x9.jpg',
+      [new Ingredient('Buns', 1), new Ingredient('Meat', 20)]
     ),
   ];
 

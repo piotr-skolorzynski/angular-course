93213262ea2bd02fb4e28cd258ced26652eb7c54
Piotr Skołorzyński <piotr.skolorzynski@aspiresys.pl>
2023-01-11 15:37:34 +0100

    course-app project set up routes

--- course-app/src/app/app-routing.module.ts
+++ course-app/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/recipes', pathMatch: 'full'},
+  {path: 'recipes', component: RecipesComponent, children: [
+    {path: ':id', component: RecipesDetailComponent}
+  ]},
+  {path: 'shopping-list', component: ShoppingListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

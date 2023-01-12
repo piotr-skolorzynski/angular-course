61ad8c8e4893217c70add14cceecba55dae08612
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-01-12 12:08:48 +0100

    make changes in course-app replace EventEmmiter in services

--- course-app/src/app/app.component.ts
+++ course-app/src/app/app.component.ts
@@ -5,11 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-course-app';
-  loadedFeature = 'recipe';
-
-  onNavigate(feature: string) {
-    this.loadedFeature = feature;
-  }
-}
+export class AppComponent {}

fb40b9f5b0196c178da4cc3dd6f53b0f39fc23a6
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-06-20 15:50:42 +0200

    use form data

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -10,6 +10,14 @@ export class AppComponent {
   defaultQuestion = 'teacher';
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: '',
+  };
+  submitted = false;
 
   suggestUserName(signupForm: NgForm) {
     const suggestedName = 'Superuser';
@@ -21,7 +29,13 @@ export class AppComponent {
     });
   }
 
-  onSubmit(form: NgForm) {
-    console.log(form);
+  onSubmit(signupForm: NgForm) {
+    this.submitted = true;
+
+    this.user.username = signupForm.value.userData.username;
+    this.user.email = signupForm.value.userData.email;
+    this.user.secretQuestion = signupForm.value.secret;
+    this.user.answer = signupForm.value.questionAnswer;
+    this.user.gender = signupForm.value.gender;
   }
 }

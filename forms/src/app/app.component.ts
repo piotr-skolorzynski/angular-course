9d3f99d14049edf0dd9a73a3c8f782520abe5932
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-06-20 14:24:18 +0200

    set default values using ngModel property binding

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -7,6 +7,8 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  defaultQuestion = 'teacher';
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }

631fff02dd9973c864e168f3f84266fb1afdddd9
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-07-07 15:47:06 +0200

    example of pipe - filter pipe

---
+++ pipes/src/app/filter.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter',
+})
+export class FilterPipe implements PipeTransform {
+  transform(value: any, filterString: string, propName: string): any {
+    //check if it is an array or search phrase is empty string
+    if (!value.length || !filterString) {
+      return value;
+    }
+
+    const resultArray = [];
+    for (const item of value) {
+      if (item[propName] === filterString) {
+        resultArray.push(item);
+      }
+    }
+
+    return resultArray;
+  }
+}

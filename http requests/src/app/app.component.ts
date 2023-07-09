55f6dda3cf8f58e3bf3128ff217ed68ad8690dbe
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-07-09 15:13:00 +0200

    send a post request

--- http requests/src/app/app.component.ts
+++ http requests/src/app/app.component.ts
@@ -4,18 +4,21 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
+  private postsURL =
+    'https://ng-complete-guide-2d4c2-default-rtdb.europe-west1.firebasedatabase.app/posts.json';
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {}
 
   onCreatePost(postData: { title: string; content: string }) {
-    // Send Http request
-    console.log(postData);
+    this.http.post(this.postsURL, postData).subscribe((responseData) => {
+      console.log(responseData);
+    });
   }
 
   onFetchPosts() {

f34703a0984f36e872e516e342d05959bcaa0b6b
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-07-09 15:21:16 +0200

    get data

--- http requests/src/app/app.component.ts
+++ http requests/src/app/app.component.ts
@@ -13,7 +13,9 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     this.http.post(this.postsURL, postData).subscribe((responseData) => {
@@ -22,10 +24,16 @@ export class AppComponent implements OnInit {
   }
 
   onFetchPosts() {
-    // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http.get(this.postsURL).subscribe((posts) => {
+      console.log(posts);
+    });
   }
 }

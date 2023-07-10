b395379986ad9dbc3a1f432ceb1713cbf413b30d
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-07-10 23:08:00 +0200

    use subjects for error handling

--- http requests/src/app/app.component.ts
+++ http requests/src/app/app.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Post } from './post.model';
 import { PostsService } from './posts.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadedPosts = [];
   isFetching = false;
   error = null;
+  private errorSub: Subscription;
 
   constructor(private http: HttpClient, private postsService: PostsService) {}
 
@@ -24,6 +26,9 @@ export class AppComponent implements OnInit {
   }
 
   onFetchPosts() {
+    this.errorSub = this.postsService.error.subscribe(
+      (errorMessage) => (this.error = errorMessage)
+    );
     this.fetchPosts();
   }
 
@@ -45,5 +50,9 @@ export class AppComponent implements OnInit {
         console.log(error);
       }
     );
+  }
+
+  ngOnDestroy(): void {
+    this.errorSub.unsubscribe();
   }
 }
--- http requests/src/app/posts.service.ts
+++ http requests/src/app/posts.service.ts
@@ -2,10 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Post } from './post.model';
 import { map } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 //można też klasycznie zarejestrować w app.module providers
 @Injectable({ providedIn: 'root' })
 export class PostsService {
+  error = new Subject<string>();
+
   constructor(private http: HttpClient) {}
 
   createAndStorePost(title: string, content: string) {
@@ -15,9 +18,14 @@ export class PostsService {
         'https://ng-complete-guide-2d4c2-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
         postData
       )
-      .subscribe((responseData) => {
-        console.log(responseData);
-      });
+      .subscribe(
+        (responseData) => {
+          console.log(responseData);
+        },
+        (error) => {
+          this.error.next(error.message);
+        }
+      );
   }
 
   fetchPosts() {

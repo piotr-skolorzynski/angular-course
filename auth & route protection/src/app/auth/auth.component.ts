dc6f5dc17e0b274b08b9a04d1c204d6c789d2e0a
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-07-18 13:41:21 +0200

    send signup request

--- auth & route protection/src/app/auth/auth.component.ts
+++ auth & route protection/src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { AuthService } from "./auth.servcie";
 
 @Component({
   selector: "app-auth",
@@ -8,12 +9,29 @@ import { NgForm } from "@angular/forms";
 export class AuthComponent {
   isLogginngMode = true;
 
+  constructor(private authService: AuthService) {}
+
   onSwitchMode() {
     this.isLogginngMode = !this.isLogginngMode;
   }
 
   onSubmit(form: NgForm) {
-    console.log(form.value);
+    if (!form.valid) {
+      return;
+    }
+
+    if (this.isLogginngMode) {
+      return;
+    }
+
+    this.authService.signup(form.value.email, form.value.password).subscribe(
+      (responseData) => {
+        console.log(responseData);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
     form.reset();
   }
 }

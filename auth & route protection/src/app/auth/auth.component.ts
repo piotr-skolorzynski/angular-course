1bf29eefd4fa9519fa4aa3cfa37ee141dea63d08
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-07-19 22:03:00 +0200

    login error handling

--- auth & route protection/src/app/auth/auth.component.ts
+++ auth & route protection/src/app/auth/auth.component.ts
@@ -1,6 +1,6 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
-import { AuthResponseData, AuthService } from "./auth.servcie";
+import { AuthResponseData, AuthService } from "./auth.service";
 import { Observable } from "rxjs";
 
 @Component({

ec2ba7b7f399e5fe979cfd0c3f711aea217d58fd
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-07-21 12:23:07 +0200

    split recipes to feature module and add routes to this module

--- optimalizations and feature modules/src/app/recipes/recipes.module.ts
+++ optimalizations and feature modules/src/app/recipes/recipes.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule } from "@angular/forms";
 
-import { RecipesComponent } from './recipes.component';
-import { RecipeListComponent } from './recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
-import { RecipeStartComponent } from './recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipesComponent } from "./recipes.component";
+import { RecipeListComponent } from "./recipe-list/recipe-list.component";
+import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
+import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { RecipesRoutingModule } from "./recipes-routing.module";
 
 @NgModule({
   declarations: [
@@ -17,16 +17,16 @@ import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
     RecipeDetailComponent,
     RecipeItemComponent,
     RecipeStartComponent,
-    RecipeEditComponent
+    RecipeEditComponent,
   ],
-  imports: [RouterModule, CommonModule, ReactiveFormsModule],
+  imports: [RecipesRoutingModule, CommonModule, ReactiveFormsModule],
   exports: [
     RecipesComponent,
     RecipeListComponent,
     RecipeDetailComponent,
     RecipeItemComponent,
     RecipeStartComponent,
-    RecipeEditComponent
-  ]
+    RecipeEditComponent,
+  ],
 })
 export class RecipesModule {}

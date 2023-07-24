38344de78d519f2b076c5a0f67373091c7ec83ed
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-07-24 14:38:52 +0200

    understanding shared module

--- optimalizations and feature modules/src/app/recipes/recipes.module.ts
+++ optimalizations and feature modules/src/app/recipes/recipes.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
 import { ReactiveFormsModule } from "@angular/forms";
 
 import { RecipesComponent } from "./recipes.component";
@@ -9,6 +8,7 @@ import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.compo
 import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
 import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
 import { RecipesRoutingModule } from "./recipes-routing.module";
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
   declarations: [
@@ -19,6 +19,6 @@ import { RecipesRoutingModule } from "./recipes-routing.module";
     RecipeStartComponent,
     RecipeEditComponent,
   ],
-  imports: [RecipesRoutingModule, CommonModule, ReactiveFormsModule],
+  imports: [RecipesRoutingModule, ReactiveFormsModule, SharedModule],
 })
 export class RecipesModule {}

7f3f1b9d73c98ad58888e16028564071ce1d7153
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-07-28 10:49:57 +0200

    the old @effect decorator and registring effects

--- ngrx/src/app/store/counter.effects.ts
+++ ngrx/src/app/store/counter.effects.ts
@@ -2,21 +2,37 @@ import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { decrement, increment } from './counter.actions';
 import { tap } from 'rxjs';
 
-export class CounterEffects {
-  saveCount = createEffect(
-    () =>
-      this.actions$.pipe(
-        //this operator lets define which actions we want to continue
-        //this will be executed always if chosen type of action is dispatched
-        ofType(increment, decrement),
-        tap((action: any) => {
-          console.log(action);
-          localStorage.setItem('count', action.value.toString());
-        })
-      ),
-    { dispatch: false }
-  );
+// export class CounterEffects {
+//   saveCount = createEffect(
+//     () =>
+//       this.actions$.pipe(
+//         //this operator lets define which actions we want to continue
+//         //this will be executed always if chosen type of action is dispatched
+//         ofType(increment, decrement),
+//         tap((action: any) => {
+//           console.log(action);
+//           localStorage.setItem('count', action.value.toString());
+//         })
+//       ),
+//     { dispatch: false }
+//   );
 
-  //dollar sign suggests that it is an observable
+//   //dollar sign suggests that it is an observable
+//   constructor(private actions$: Actions) {}
+// }
+
+export class CounterEffects {
+  @Effect({ dispatch: false })
+  saveCount = () =>
+    this.actions$.pipe(
+      //this operator lets define which actions we want to continue
+      //this will be executed always if chosen type of action is dispatched
+      ofType(increment, decrement),
+      tap((action: any) => {
+        console.log(action);
+        localStorage.setItem('count', action.value.toString());
+      })
+    );
+
   constructor(private actions$: Actions) {}
 }

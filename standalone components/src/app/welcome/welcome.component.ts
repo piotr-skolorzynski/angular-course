87a5a59b5e7024a50965aeaaefb027a2945d99eb
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-08-01 19:44:28 +0200

    build a first standalone component

--- standalone components/src/app/welcome/welcome.component.ts
+++ standalone components/src/app/welcome/welcome.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
-  templateUrl: './welcome.component.html'
+  templateUrl: './welcome.component.html',
 })
 export class WelcomeComponent {}

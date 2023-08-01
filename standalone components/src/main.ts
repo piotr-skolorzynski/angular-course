1d780622fed98eb2bfaf57e9d901595c88a11d58
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-08-01 20:23:08 +0200

    services and standalone components

--- standalone components/src/main.ts
+++ standalone components/src/main.ts
@@ -3,9 +3,12 @@ import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { AnalyticsService } from './app/shared/analytics.service';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent);
+bootstrapApplication(AppComponent, {
+  providers: [AnalyticsService],
+});

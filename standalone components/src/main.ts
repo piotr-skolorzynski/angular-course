455b002938f95d898c8d763a04a915254bffaed5
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-08-02 20:29:12 +0200

    routing

--- standalone components/src/main.ts
+++ standalone components/src/main.ts
@@ -1,14 +1,15 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 
 import { environment } from './environments/environment';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { AnalyticsService } from './app/shared/analytics.service';
+import { AppRoutingModule } from './app/app-routing.module';
 
 if (environment.production) {
   enableProdMode();
 }
 
 bootstrapApplication(AppComponent, {
-  providers: [AnalyticsService],
+  providers: [AnalyticsService, importProvidersFrom(AppRoutingModule)],
 });

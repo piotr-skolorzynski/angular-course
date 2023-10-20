8151bfe4a1c1991577efe937d7002c15c79479dd
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-10-20 13:44:49 +0200

    prepare form to exercise reactive aproach

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -7,35 +7,5 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  defaultQuestion = 'teacher';
-  answer = '';
   genders = ['male', 'female'];
-  user = {
-    username: '',
-    email: '',
-    secretQuestion: '',
-    answer: '',
-    gender: '',
-  };
-  submitted = false;
-
-  suggestUserName(form: NgForm) {
-    const suggestedName = 'Superuser';
-    form.form.patchValue({
-      userData: {
-        username: suggestedName,
-      },
-    });
-  }
-
-  onSubmit(form: NgForm) {
-    this.submitted = true;
-    this.user.username = form.form.value.userData.username;
-    this.user.email = form.form.value.userData.email;
-    this.user.secretQuestion = form.form.value.secret;
-    this.user.answer = form.form.value.questionAnswer;
-    this.user.gender = form.form.value.gender;
-
-    form.reset();
-  }
 }

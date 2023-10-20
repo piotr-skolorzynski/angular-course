3a4ff30b1cc78993082f177eb9fa0ea351d80852
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-10-20 08:56:47 +0200

    forms again

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -1,43 +1,12 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  defaultQuestion = 'teacher';
-  answer = '';
-  genders = ['male', 'female'];
-  user = {
-    username: '',
-    email: '',
-    secretQuestion: '',
-    answer: '',
-    gender: '',
-  };
-  submitted = false;
-
-  suggestUserName(signupForm: NgForm) {
+  suggestUserName() {
     const suggestedName = 'Superuser';
-
-    signupForm.form.patchValue({
-      userData: {
-        username: suggestedName,
-      },
-    });
-  }
-
-  onSubmit(signupForm: NgForm) {
-    this.submitted = true;
-
-    this.user.username = signupForm.value.userData.username;
-    this.user.email = signupForm.value.userData.email;
-    this.user.secretQuestion = signupForm.value.secret;
-    this.user.answer = signupForm.value.questionAnswer;
-    this.user.gender = signupForm.value.gender;
-
-    signupForm.reset();
   }
 }

2bf462632f3b3b6cf3f8788e6f67231c549d0c2c
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-10-20 13:33:17 +0200

    finish template driven form

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  suggestUserName() {
+  defaultQuestion = 'teacher';
+  answer = '';
+  genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: '',
+  };
+  submitted = false;
+
+  suggestUserName(form: NgForm) {
     const suggestedName = 'Superuser';
+    form.form.patchValue({
+      userData: {
+        username: suggestedName,
+      },
+    });
+  }
+
+  onSubmit(form: NgForm) {
+    this.submitted = true;
+    this.user.username = form.form.value.userData.username;
+    this.user.email = form.form.value.userData.email;
+    this.user.secretQuestion = form.form.value.secret;
+    this.user.answer = form.form.value.questionAnswer;
+    this.user.gender = form.form.value.gender;
+
+    form.reset();
   }
 }

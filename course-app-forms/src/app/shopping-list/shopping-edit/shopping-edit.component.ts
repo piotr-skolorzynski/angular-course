984b8b1b0c05cc139ca5d13b5d4e0674f3b4d725
Piotr Skołorzyński <piotr.skolorzynski@aspiresys.pl>
2023-10-23 10:40:10 +0200

    enable to edit product from the list

--- course-app-forms/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ course-app-forms/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css'],
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
   addItemForm: FormGroup;
+  subscription: Subscription;
+  editMode = false;
+  editedItemIndex: number;
+  editedItem: Ingredient;
 
   constructor(private slService: ShoppingListService) {}
 
@@ -19,6 +24,18 @@ export class ShoppingEditComponent implements OnInit {
       productName: new FormControl(null, Validators.required),
       productAmount: new FormControl(null, Validators.required),
     });
+
+    this.subscription = this.slService.startedEditing.subscribe(
+      (index: number) => {
+        this.editedItemIndex = index;
+        this.editMode = true;
+        this.editedItem = this.slService.getIngredient(index);
+        this.addItemForm.setValue({
+          productName: this.editedItem.name,
+          productAmount: this.editedItem.amount,
+        });
+      }
+    );
   }
 
   onAddItem() {
@@ -27,5 +44,9 @@ export class ShoppingEditComponent implements OnInit {
     const product = new Ingredient(productName, productAmount);
 
     this.slService.addIngredient(product);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }

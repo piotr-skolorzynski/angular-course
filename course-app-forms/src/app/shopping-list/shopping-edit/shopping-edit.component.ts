66da0dca01e3506a45713ce7cf70d2655a6cd4ea
Piotr Skołorzyński <piotr.skolorzynski@aspiresys.pl>
2023-10-23 10:19:16 +0200

    add reactive form aproach add validation enable to add product to the list

--- course-app-forms/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ course-app-forms/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,32 +1,31 @@
-import {
-  Component,
-  OnInit,
-  ElementRef,
-  ViewChild
-} from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
-  @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
+  addItemForm: FormGroup;
 
-  constructor(private slService: ShoppingListService) { }
+  constructor(private slService: ShoppingListService) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.addItemForm = new FormGroup({
+      productName: new FormControl(null, Validators.required),
+      productAmount: new FormControl(null, Validators.required),
+    });
   }
 
   onAddItem() {
-    const ingName = this.nameInputRef.nativeElement.value;
-    const ingAmount = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(ingName, ingAmount);
-    this.slService.addIngredient(newIngredient);
-  }
+    const productName = this.addItemForm.value.productName;
+    const productAmount = this.addItemForm.value.productAmount;
+    const product = new Ingredient(productName, productAmount);
 
+    this.slService.addIngredient(product);
+  }
 }

6823eac477100427212d1442a228d97805ceff1e
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-10-24 10:08:40 +0200

    validate inputs

--- course-app-forms/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ course-app-forms/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RecipeService } from '../recipe.service';
 
@@ -35,8 +35,11 @@ export class RecipeEditComponent implements OnInit {
   onAddIngredient() {
     (<FormArray>this.recipeForm.get('ingredients')).push(
       new FormGroup({
-        name: new FormControl(null),
-        amount: new FormControl(null),
+        name: new FormControl(null, Validators.required),
+        amount: new FormControl(null, [
+          Validators.required,
+          Validators.pattern(/^[1-9]+[0-9]*$/),
+        ]),
       })
     );
   }
@@ -50,16 +53,19 @@ export class RecipeEditComponent implements OnInit {
         for (let ingredient of recipe.ingredients) {
           recipeIngredients.push(
             new FormGroup({
-              name: new FormControl(ingredient.name),
-              amount: new FormControl(ingredient.amount),
+              name: new FormControl(ingredient.name, Validators.required),
+              amount: new FormControl(ingredient.amount, [
+                Validators.required,
+                Validators.pattern(/^[1-9]+[0-9]*$/),
+              ]),
             })
           );
         }
       }
       this.recipeForm = new FormGroup({
-        name: new FormControl(recipe.name),
-        imagePath: new FormControl(recipe.imagePath),
-        description: new FormControl(recipe.description),
+        name: new FormControl(recipe.name, Validators.required),
+        imagePath: new FormControl(recipe.imagePath, Validators.required),
+        description: new FormControl(recipe.description, Validators.required),
         ingredients: recipeIngredients,
       });
 
@@ -67,9 +73,9 @@ export class RecipeEditComponent implements OnInit {
     }
 
     this.recipeForm = new FormGroup({
-      name: new FormControl(null),
-      imagePath: new FormControl(null),
-      description: new FormControl(null),
+      name: new FormControl(null, Validators.required),
+      imagePath: new FormControl(null, Validators.required),
+      description: new FormControl(null, Validators.required),
       ingredients: recipeIngredients,
     });
   }
